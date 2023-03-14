090cb8c54a2fba83435d001486270e45bfacd2d4
asman <asman@codingveda.in>
2023-03-15 02:10:28 +0530

    minor changes.

--- scripts/fetch_submodules.sh
+++ scripts/fetch_submodules.sh
@@ -1,4 +1,9 @@
-cd ../source/third_party &&
+#Check for third_party folder and doesn't exist then create it.
+DIRECTORY="source/third_party"
+if [ ! -d "$DIRECTORY" ]; then
+  mkdir $DIRECTORY
+fi
+cd source/third_party &&
 git clone --recurse-submodules https://github.com/drogonframework/drogon &&
 git clone --recurse-submodules https://github.com/open-source-parsers/jsoncpp &&
 git clone --recurse-submodules https://github.com/Thalhammer/jwt-cpp &&
