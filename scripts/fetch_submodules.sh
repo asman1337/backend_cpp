0865b52df6e23fbc45676cf9aae24bcdb0feb5bb
asman <asman@codingveda.in>
2023-03-20 03:56:53 +0530

    added poco repo.

--- scripts/fetch_submodules.sh
+++ scripts/fetch_submodules.sh
@@ -3,8 +3,10 @@ DIRECTORY="source/third_party"
 if [ ! -d "$DIRECTORY" ]; then
   mkdir $DIRECTORY
 fi
+
 cd source/third_party &&
 git clone --recurse-submodules https://github.com/drogonframework/drogon &&
 git clone --recurse-submodules https://github.com/open-source-parsers/jsoncpp &&
 git clone --recurse-submodules https://github.com/Thalhammer/jwt-cpp &&
-git clone --recurse-submodules https://github.com/trusch/libbcrypt
+git clone --recurse-submodules https://github.com/trusch/libbcrypt &&
+git clone -b master https://github.com/pocoproject/poco.git
